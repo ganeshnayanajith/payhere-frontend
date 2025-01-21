24e306dda76680dc97589afd40509697ed378767
Ganesh Nayanajith <ganeshnayanajith40@gmail.com>
2025-01-21 21:05:28 +0530

    frontend test app for payhere

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import PaymentButton from "./components/PaymentButton";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>PayHere Payment Integration</h1>
+      <PaymentButton />
+    </div>
+  );
+}
+
+export default App;
